e7f47229362c7eb0255a61cf2711b377974f0614
LM-Home <90747587+LM-Home@users.noreply.github.com>
2023-08-26 05:24:33 +0800

    Update and rename diy-part1.sh to diy-feed.sh

--- diy-feed.sh
+++ diy-feed.sh
@@ -11,7 +11,11 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#luci-theme-argon 
+rm -rf feeds/luci/themes/luci-theme-argon  
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon 
